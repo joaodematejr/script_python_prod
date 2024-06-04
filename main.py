14f9f307cde221df60913a732ecfe0bc8f7b6d58
João Dematé Jr <joaodematejr@gmail.com>
2024-06-04 18:22:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ with open(json_file_path, 'w', encoding='utf-8') as json_file:
     json_file.write(json_data)
 
 print(f'Arquivo JSON salvo em: {json_file_path}')
+
+# Gerar comandos SQL INSERT INTO
+insert_statements = []
+
+for index, row in df.iterrows():
+    insert_statement = f"INSERT INTO tb_member (name, cpf, plano) VALUES ('{row['nome']}', '{row['cpf']}', '{row['plano']}');"
+    insert_statements.append(insert_statement)
+
+# Salvar os comandos SQL em um arquivo
+sql_file_path = 'inserts.sql'
+with open(sql_file_path, 'w', encoding='utf-8') as sql_file:
+    for statement in insert_statements:
+        sql_file.write(statement + '\n')
+
+print(f'Arquivo SQL salvo em: {sql_file_path}')
